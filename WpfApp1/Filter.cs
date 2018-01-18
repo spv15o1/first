7aa51f51af83abd0e980259c0c14638c343daae1
Serhii <sergey.zorya@gmail.com>
2018-01-18 18:29:38 +0200

    Add new project
    Add new project

---
+++ WpfApp1/Filter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public class Filter
+    {
+        public FilterType FilterType{get;set;}
+
+        public Image SetFilter(Image source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp1/FilterType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApp1
+{
+    public enum FilterType
+    {
+        Red,
+        Blue,
+        Green
+    }
+}
