0bcbd982d0e1e41aae50e0c6db342251e2a0180d
sergey.zorya <sergey.zorya@gmail.com>
2018-01-22 18:36:11 +0200

    Add message box
    Add message box

--- WpfApp1/UploadPhoto.cs
+++ WpfApp1/UploadPhoto.cs
@@ -23,14 +23,12 @@ namespace WpfApp1
             for(int i=0; i<files.Length; i++)
             {
                  images.Add(new BitmapImage(new Uri(files[i])));
-            } 
+            }
+            MessageBox.Show("Изображения добавлены");
 
-           
         }
         static public void OpenFile()
         {
-        
-
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Multiselect = true;
             ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
@@ -40,7 +38,7 @@ namespace WpfApp1
                 images.Add(new BitmapImage(new Uri(v)));
 
             }
-
+            MessageBox.Show("Изображения добавлены");
         }
 
 
